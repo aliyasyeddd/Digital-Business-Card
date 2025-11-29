ae9086a546e90ebcb0e7b4928c06aff45b430a37
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-29 20:49:19 +0530

    Add profile image and styled name to About page
    Introduced a profile image and styled name to the About component. Updated App.css with new styles for the profile picture and name, and increased the border-radius for the main container.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Profile from '../assets/Aliya.jpg'
 
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <img className='profile-pic' src={Profile} alt="Aliya Profile" />
+      <h1 className='profile-name'>Aliya Syed</h1>
     </div>
   )
 }
