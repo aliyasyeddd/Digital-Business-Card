8f4781a98fafdf04fc4710a18595e34d8e881df7
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-29 18:36:07 +0530

    Add business card assets and main React components
    Replaced favicon and title in index.html, added a new SVG business card asset, removed the old vite.svg, and introduced main React components (About, Footer, Info, Interests) along with a profile image. Updated App.jsx to use the new components and assets.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 
 import './App.css'
+import About from './Components/About'
+import Info from './Components/info'
+import Interests from './Components/Interests'
+import Footer from './Components/Footer'
+
 
 function App() {
- 
+   return (
+    <> 
+      <About />
+      <Info />
+      <Interests />
+      <Footer />
+    </>
+   )
 
 }
 
