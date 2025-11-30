db3345ead5a733fce560cce12eb6c8585e4fae51
aliya mahaboob <mahaboobaliya02@gmail.com>
2025-11-30 05:33:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import About from "./Components/About";
-import Info from "./Components/Info";
+import Info from "./Components/info";
 import Interests from "./Components/Interests";
 import Footer from "./Components/Footer";
 
